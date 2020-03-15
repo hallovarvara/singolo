81f637f98876cbe50157ab203f2f40de14ad2683
varya <hallovarvara@gmail.com>
2020-03-16 00:14:49 +0300

    feat: add slider interactivity

--- script.js
+++ script.js
@@ -16,6 +16,70 @@ menu.addEventListener("click", (event) => {
 
 
 
+/* SLIDER */
+
+const sliderBackground = document.querySelector(".slider");
+const slideContainer = document.querySelector(".slider__images");
+const arrow = document.querySelectorAll(".slider .arrow");
+
+const slides = {
+  0: [`<div class="iphone iphone_vertical">
+        <div class="iphone__screen"></div>
+        <div class="iphone__phone"></div>
+        <div class="iphone__shadow"></div>
+       </div>`,
+      `<div class="iphone iphone_horizontal">
+        <div class="iphone__screen"></div>
+        <div class="iphone__phone"></div>
+        <div class="iphone__shadow"></div>
+       </div>`],
+  1: [`<img width="517" height="513" src="./assets/slider-images/2-iphones.png" alt="iPhone Vertical">`]
+};
+
+const slideColor = {
+  0: "bg-red",
+  1: "bg-blue",
+  "default": "bg-red"
+}
+
+// Phone screens switching off / on
+function phoneScreensActivate() {
+  const phones = document.querySelectorAll(".slider .iphone__screen");
+  phones.forEach( phone => phone.addEventListener( "click", event => {
+    if ( event.target.classList.contains("hidden") ) {
+      event.target.classList.remove("hidden");
+    } else {
+      event.target.classList.add("hidden");
+    }
+  }));
+}
+phoneScreensActivate();
+
+let currentSlide = 0;
+arrow.forEach( each => each.addEventListener("click", event => {
+  ( event.target.classList.contains("left") ) ? currentSlide-- : currentSlide++;
+
+  if (currentSlide == Object.keys(slides).length) {
+    currentSlide = 0;
+  } else if (currentSlide < 0) {
+    currentSlide = Object.keys(slides).length-1;
+  }
+
+  sliderBackground.classList.value = "slider home";
+
+  if(slideColor.hasOwnProperty(currentSlide)) {
+    sliderBackground.classList.add(slideColor[currentSlide]);
+  } else {
+    sliderBackground.classList.add( slideColor["default"] );
+  }
+
+  slideContainer.innerHTML = "";
+  slides[currentSlide].forEach( img => slideContainer.innerHTML += `\n${img}`);
+
+  phoneScreensActivate();
+}));
+
+
 /* PORTFOLIO */
 
 const tags = document.querySelector(".portfolio__tags");
@@ -23,22 +87,30 @@ const portfolio = document.querySelector(".portfolio__pictures");
 
 // Shift portfolio pictures by clicking on tag
 tags.addEventListener("click", (event) => {
+  // Prevent selected tag from click action
+  if( !event.target.classList.contains("selected") ) {
+    let portfolioPictures = [...portfolio.querySelectorAll(".portfolio__picture")];
+    portfolioPictures.unshift(portfolioPictures.pop());
+    portfolioPictures.forEach( pic => portfolio.append(pic) );
+  }
   tags.querySelectorAll("button").forEach(e => e.classList.remove('selected'));
   event.target.classList.add("selected");
-
-  let portfolioPictures = [...portfolio.querySelectorAll(".portfolio__picture")];
-
-  portfolioPictures.unshift(portfolioPictures.pop());
-
-  portfolioPictures.forEach( pic => {
-    portfolio.append(pic);
-  });
 });
 
 // Make clicked portfolio picture bordered
+let switchNow = true;
 portfolio.addEventListener("click", (event) => {
+  if ( event.target.classList.contains("bordered") ) {
+    switchNow = false;
+  }
+
   portfolio.querySelectorAll("img").forEach(pic => pic.classList.remove("bordered"));
-  event.target.classList.add("bordered");
+  
+  if (switchNow) {
+    event.target.classList.add("bordered");
+  }
+
+  switchNow = true;
 });
 
 
@@ -99,4 +171,4 @@ button.addEventListener("click", (event) => {
 });
 
 // Add close action to modal window background
-// modalBackground.addEventListener("click", hideModal);
+modalBackground.addEventListener("click", hideModal);
